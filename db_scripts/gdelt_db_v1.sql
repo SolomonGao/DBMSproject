4458061e2143356108bab27f33f7c41b3b1d2270
Xing gao <738234123@qq.com>
2026-04-01 17:06:59 -0400

    1

--- db_scripts/gdelt_db_v1.sql
+++ db_scripts/gdelt_db_v1.sql
@@ -1,37 +1,37 @@
 -- 创建数据库
 CREATE DATABASE IF NOT EXISTS gdelt;
-USE gdelt_db;
+USE gdelt;
 
--- 创建事件表
-CREATE TABLE events_table (
-    GlobalEventID BIGINT PRIMARY KEY,
-    SQLDATE DATE NOT NULL,
-    MonthYear INT,
-    DATEADDED DATETIME,
-    Actor1Name VARCHAR(255),
-    Actor1CountryCode CHAR(3),
-    Actor1Type1Code VARCHAR(10),
-    Actor2Name VARCHAR(255),
-    Actor2CountryCode CHAR(3),
-    Actor2Type1Code VARCHAR(10),
-    EventCode VARCHAR(10),
-    EventRootCode VARCHAR(10),
-    QuadClass INT,
-    GoldsteinScale FLOAT,
-    AvgTone FLOAT,
-    NumArticles INT,
-    NumMentions INT,
-    NumSources INT,
-    ActionGeo_Type INT,
-    ActionGeo_FullName TEXT,
-    ActionGeo_CountryCode CHAR(2),
-    ActionGeo_Lat DECIMAL(10, 7),
-    ActionGeo_Long DECIMAL(10, 7),
-    SOURCEURL TEXT,
+-- -- 创建事件表
+-- CREATE TABLE events_table (
+--     GlobalEventID BIGINT PRIMARY KEY,
+--     SQLDATE DATE NOT NULL,
+--     MonthYear INT,
+--     DATEADDED DATETIME,
+--     Actor1Name VARCHAR(255),
+--     Actor1CountryCode CHAR(3),
+--     Actor1Type1Code VARCHAR(10),
+--     Actor2Name VARCHAR(255),
+--     Actor2CountryCode CHAR(3),
+--     Actor2Type1Code VARCHAR(10),
+--     EventCode VARCHAR(10),
+--     EventRootCode VARCHAR(10),
+--     QuadClass INT,
+--     GoldsteinScale FLOAT,
+--     AvgTone FLOAT,
+--     NumArticles INT,
+--     NumMentions INT,
+--     NumSources INT,
+--     ActionGeo_Type INT,
+--     ActionGeo_FullName TEXT,
+--     ActionGeo_CountryCode CHAR(2),
+--     ActionGeo_Lat DECIMAL(10, 7),
+--     ActionGeo_Long DECIMAL(10, 7),
+--     SOURCEURL TEXT,
 
 
-    ActionGeo_Point POINT NOT NULL SRID 4326 
-) ENGINE=InnoDB;
+--     ActionGeo_Point POINT NOT NULL SRID 4326 
+-- ) ENGINE=InnoDB;
 
 
 -- Insert data first after create and then run those below
@@ -50,4 +50,4 @@ CREATE TABLE events_table (
 -- ALTER TABLE events_table ADD INDEX idx_lat (ActionGeo_Lat);
 -- ALTER TABLE events_table ADD INDEX idx_long (ActionGeo_Long);
 
--- ALTER TABLE events_table ADD SPATIAL INDEX idx_geo_point (ActionGeo_Point);
+ALTER TABLE events_table ADD SPATIAL INDEX idx_geo_point (ActionGeo_Point);
